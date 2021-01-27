3fd64eae5b9ecb022a35668b58281f12693d6c62
Joker <459506421@qq.com>
2021-01-27 17:12:34 +0800

    demo分类调整

--- Demo1/js/demo2.js
+++
@@ -1,9 +0,0 @@
-function testFormatXML() {
-    var str = $("#inputStr").val();
-    var formatStr = formatXml(str);
-    // remove multiple newlines
-    var reg = /(\r\n\r\n)/g;
-    formatStr = formatStr.replace(reg, '\r\n');
-    $("#outputStr").val(formatStr);
-    console.log();
-}
